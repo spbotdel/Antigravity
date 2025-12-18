5bd2c9a43160ac9887d568ed67f000bb761d87c9
spbotdel <79367368+spbotdel@users.noreply.github.com>
2025-12-18 23:06:37 +0300

    Add files via upload

---
+++ js/treeBuilder.js
@@ -0,0 +1,27 @@
+export function buildDescendantsTree(personId, data, visited = new Set()) {
+  if (visited.has(personId)) return null;
+  visited.add(personId);
+
+  const person = data.people[personId];
+  if (!person) return null;
+
+  const node = {
+    id: person.id,
+    name: person.name,
+    birth: person.birth,
+    death: person.death,
+    children: []
+  };
+
+  person.familiesAsSpouse.forEach(famId => {
+    const fam = data.families[famId];
+    if (!fam) return;
+
+    fam.children.forEach(childId => {
+      const childNode = buildDescendantsTree(childId, data, visited);
+      if (childNode) node.children.push(childNode);
+    });
+  });
+
+  return node;
+}
